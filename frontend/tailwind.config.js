bf1561d45307e72411589b970b204b5d763c2447
keshav kasera <anonymous_00@keshavs-MacBook-Air.local>
2025-08-20 22:56:37 +0530

    initialized frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,31 @@
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#1e40af", // brand blue
+          light: "#3b82f6",
+          dark: "#1e3a8a",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
