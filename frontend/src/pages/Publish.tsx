448e6d4270f3859d79c4c22cdef92ecaca51e6b4
keshav kasera <anonymous_00@keshavs-MacBook-Air.local>
2025-09-04 22:13:38 +0530

    frontend updated

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -15,7 +15,7 @@ export const Publish = () => {
             <div className="max-w-screen-lg w-full">
                 <input onChange={(e) => {
                     setTitle(e.target.value)
-                }} type="text" className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Title" />
+                }} type="text" className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5" placeholder="Title" />
 
                 <TextEditor onChange={(e) => {
                     setDescription(e.target.value)
@@ -42,7 +42,7 @@ export const Publish = () => {
 function TextEditor({ onChange }: {onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void}) {
     return <div className="mt-2">
         <div className="w-full mb-4 ">
-            <div className="flex items-center justify-between border">
+                <div className="flex items-center justify-between border">
                 <div className="my-2 bg-white rounded-b-lg w-full">
                     <label className="sr-only">Publish post</label>
                     <textarea onChange={onChange} id="editor" rows={8} className="focus:outline-none block w-full px-0 text-sm text-gray-800 bg-white border-0 pl-2" placeholder="Write an article..." required />
